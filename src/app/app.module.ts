e564d1e0866dda0704b1f60230d461a6cc450e97
Saeree <saeree_club@hotmail.co.th>
2017-11-13 13:31:43 +0700

    cretae new test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './pages/test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
